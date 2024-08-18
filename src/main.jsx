37e9d701cec7a02695eb0ce7b318fb501230d482
Kevin Lin <112509745+kev1n1in@users.noreply.github.com>
2024-08-18 15:32:30 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from './CatFacts.jsx'
 
 const queryClient = new QueryClient();
